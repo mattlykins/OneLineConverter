86bbabe306b913b7fcaf4a7c1f7fae582800a782
mattlykins <matt@mattlykins.com>
2013-07-29 22:07:31 -0400

    Remove unused imports

--- onelineconverter/src/com/mattlykins/onelineconverter/ViewDB.java
+++ onelineconverter/src/com/mattlykins/onelineconverter/ViewDB.java
@@ -1,14 +1,10 @@
 package com.mattlykins.onelineconverter;
 
-import com.mattlykins.onelineconverter.dbContract.dBase;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
-import android.view.ViewDebug.FlagToString;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 

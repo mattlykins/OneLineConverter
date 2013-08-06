7542082849ce98720d1203360b9ade09fd99b874
mattlykins <matt@mattlykins.com>
2013-08-05 22:58:37 -0400

    Refactor methods to helper class

---
+++ onelineconverter/src/com/mattlykins/onelineconverter/DBFunctions.java
@@ -0,0 +1,279 @@
+package com.mattlykins.onelineconverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mattlykins.onelineconverter.dbContract.dBase;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DBFunctions
+{
+	public Context context;
+	public dbHelper mydbHelper;
+
+	public DBFunctions(Context context, dbHelper mydbHelper)
+	{
+		this.context = context;
+		this.mydbHelper = mydbHelper;
+	}
+
+	List<Convs> AddToList(Cursor cursor)
+	{
+		List<Convs> List1 = new ArrayList<Convs>();
+		for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext())
+		{
+			if (cursor != null)
+			{
+				boolean lgAddToList = true;
+				Convs conv = new Convs(cursor.getString(dBase.NDEX_ID), cursor.getString(dBase.NDEX_FROMSYMBOL), cursor.getString(dBase.NDEX_FROMTEXT),
+						cursor.getString(dBase.NDEX_TOSYMBOL), cursor.getString(dBase.NDEX_TOTEXT), cursor.getString(dBase.NDEX_MULTIBY));
+				
+				if (lgAddToList)
+				{
+					Log.d("FERRET",
+							cursor.getString(dBase.NDEX_ID) + " " + cursor.getString(dBase.NDEX_FROMSYMBOL) + " " + cursor.getString(dBase.NDEX_FROMTEXT) + " "
+									+ cursor.getString(dBase.NDEX_TOSYMBOL) + " " + cursor.getString(dBase.NDEX_TOTEXT) + " "
+									+ cursor.getString(dBase.NDEX_MULTIBY) + "\n");
+					List1.add(conv);
+				}
+			}
+
+		}
+		return List1;
+	}
+	
+	public boolean IntegrityTest()
+	{
+		//Create a list of all of the conversions
+		List<Convs> AllConvs = new ArrayList<Convs>();
+		AllConvs = mydbHelper.getAllConvs();				
+		
+		//Loop through each conversion and verify that an inverse conversion exists with the correct factor
+		for (Convs C: AllConvs )
+		{			
+			
+			
+		}
+		
+		
+		
+		
+		
+		return false;
+	}
+
+	double FindFactorWrapper(MainActivity act, String sFromUnit, String sToUnit)
+	{
+		// Verify the unit conversion is potentially possible
+		Cursor testFF = mydbHelper.searchFrom(sFromUnit, null);
+		Cursor testTF = mydbHelper.searchTo(sFromUnit, null);
+		if (testFF == null && testTF == null)
+		{
+			Toast toast = Toast.makeText(act, "Invalid From Unit", Toast.LENGTH_SHORT);
+			toast.show();
+			return 0;
+		}
+
+		Cursor testFT = mydbHelper.searchFrom(sToUnit, null);
+		Cursor testTT = mydbHelper.searchTo(sToUnit, null);
+		if (testFT == null && testTT == null)
+		{
+			Toast toast = Toast.makeText(act, "Invalid To Unit", Toast.LENGTH_SHORT);
+			toast.show();
+			return 0;
+		}
+
+		List<Integer> IDS = new ArrayList<Integer>();
+
+		Log.d("FERRET", "Calling junk with " + sFromUnit + " " + sToUnit + "\n");
+		return (FindFactor(act, sFromUnit, sToUnit));
+	}
+
+	double FindFactor(MainActivity act, String sFromUnit, String sToUnit)
+	{
+		// Create a list of convs where the From unit in the db query is the
+		// user entered from unit
+		List<Convs> FF = new ArrayList<Convs>();
+		Cursor cursorFF = mydbHelper.searchFrom(sFromUnit, null);
+		if (cursorFF != null)
+		{
+			FF = AddToList(cursorFF);
+			cursorFF.close();
+
+			// Check the array of convs to see if any of the dB To units equal
+			// the user entered To Unit
+			for (Convs C : FF)
+			{
+				if (C.getToSymbol().equals(sToUnit))
+				{
+					return Double.parseDouble(C.getMultiBy());
+				}
+
+			}
+		}
+
+		// Query the user To unit from the To side of db
+		List<Convs> TT = new ArrayList<Convs>();
+		Cursor cursorTT = mydbHelper.searchTo(sToUnit, null);
+		if (cursorTT != null)
+		{
+			TT = AddToList(cursorTT);
+			cursorTT.close();
+
+			for (Convs L : FF)
+			{
+				for (Convs R : TT)
+				{
+					if (L.getToSymbol().equals(R.getFromSymbol()))
+					{
+
+						return Double.parseDouble(L.getMultiBy()) * Double.parseDouble(R.getMultiBy());
+					}
+
+				}
+			}
+		}
+
+		// The three step
+		for (Convs L : FF)
+		{
+			List<Convs> FF2 = new ArrayList<Convs>();
+			Cursor cursorFF2 = mydbHelper.searchFrom(L.getToSymbol(), null);
+			if (cursorFF2 != null)
+			{
+				FF2 = AddToList(cursorFF2);
+				cursorFF2.close();
+				for (Convs R : TT)
+				{
+					for (Convs L2 : FF2)
+					{
+						if (L.getToSymbol().equals(L2.getFromSymbol()) && R.getFromSymbol().equals(L2.getToSymbol()))
+						{
+							Log.d("THREESTEP", L.getFromSymbol() + " " + L.getFromText() + " " + L.getToSymbol() + " " + L.getToText() + " " + L.getMultiBy()
+									+ "\n");
+							Log.d("THREESTEP",
+									L2.getFromSymbol() + " " + L2.getFromText() + " " + L2.getToSymbol() + " " + L2.getToText() + " " + L2.getMultiBy() + "\n");
+							Log.d("THREESTEP", R.getFromSymbol() + " " + R.getFromText() + " " + R.getToSymbol() + " " + R.getToText() + " " + R.getMultiBy()
+									+ "\n");
+
+							return Double.parseDouble(L.getMultiBy()) * Double.parseDouble(R.getMultiBy()) * Double.parseDouble(L2.getMultiBy());
+						}
+					}
+				}
+			}
+		}
+
+		// The four step
+		for (Convs L : FF)
+		{
+			List<Convs> FF2 = new ArrayList<Convs>();
+			Cursor cursorFF2 = mydbHelper.searchFrom(L.getToSymbol(), null);
+			if (cursorFF2 != null)
+			{
+				FF2 = AddToList(cursorFF2);
+				cursorFF2.close();
+				for (Convs R : TT)
+				{
+					List<Convs> TT2 = new ArrayList<Convs>();
+					Cursor cursorTT2 = mydbHelper.searchTo(R.getFromSymbol(), null);
+					if (cursorTT2 != null)
+					{
+						TT2 = AddToList(cursorTT2);
+						cursorTT2.close();
+						for (Convs L2 : FF2)
+						{
+							for (Convs R2 : TT2)
+							{
+								if (L.getToSymbol().equals(L2.getFromSymbol()) && R.getFromSymbol().equals(R2.getToSymbol())
+										&& L2.getToSymbol().equals(R2.getFromSymbol()))
+								{
+									Log.d("FOURSTEP",
+											L.getFromSymbol() + " " + L.getFromText() + " " + L.getToSymbol() + " " + L.getToText() + " " + L.getMultiBy()
+													+ "\n");
+									Log.d("FOURSTEP",
+											L2.getFromSymbol() + " " + L2.getFromText() + " " + L2.getToSymbol() + " " + L2.getToText() + " " + L2.getMultiBy()
+													+ "\n");
+									Log.d("FOURSTEP",
+											R2.getFromSymbol() + " " + R2.getFromText() + " " + R2.getToSymbol() + " " + R2.getToText() + " " + R2.getMultiBy()
+													+ "\n");
+									Log.d("FOURSTEP",
+											R.getFromSymbol() + " " + R.getFromText() + " " + R.getToSymbol() + " " + R.getToText() + " " + R.getMultiBy()
+													+ "\n");
+
+									return Double.parseDouble(L.getMultiBy()) * Double.parseDouble(R.getMultiBy()) * Double.parseDouble(L2.getMultiBy())
+											* Double.parseDouble(R2.getMultiBy());
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// The Five Step
+		for (Convs L : FF)
+		{
+			List<Convs> FF2 = new ArrayList<Convs>();
+			Cursor cursorFF2 = mydbHelper.searchFrom(L.getToSymbol(), null);
+			if (cursorFF2 != null)
+			{
+				FF2 = AddToList(cursorFF2);
+				cursorFF2.close();
+				for (Convs R : TT)
+				{
+					List<Convs> TT2 = new ArrayList<Convs>();
+					Cursor cursorTT2 = mydbHelper.searchTo(R.getFromSymbol(), null);
+					if (cursorTT2 != null)
+					{
+						TT2 = AddToList(cursorTT2);
+						cursorTT2.close();
+						for (Convs L2 : FF2)
+						{
+							List<Convs> FF3 = new ArrayList<Convs>();
+							Cursor cursorFF3 = mydbHelper.searchFrom(L2.getToSymbol(), null);
+							if (cursorFF3 != null)
+							{
+								FF3 = AddToList(cursorFF3);
+								cursorFF3.close();
+
+								for (Convs R2 : TT2)
+								{
+									for (Convs L3 : FF3)
+									{
+
+										if (L.getToSymbol().equals(L2.getFromSymbol()) && R.getFromSymbol().equals(R2.getToSymbol())
+												&& L2.getToSymbol().equals(L3.getFromSymbol()) && L3.getToSymbol().equals(R2.getFromSymbol()))
+										{
+											Log.d("FIVESTEP",
+													L.getFromSymbol() + " " + L.getFromText() + " " + L.getToSymbol() + " " + L.getToText() + " "
+															+ L.getMultiBy() + "\n");
+											Log.d("FIVESTEP", L2.getFromSymbol() + " " + L2.getFromText() + " " + L2.getToSymbol() + " " + L2.getToText() + " "
+													+ L2.getMultiBy() + "\n");
+											Log.d("FIVESTEP", L3.getFromSymbol() + " " + L3.getFromText() + " " + L3.getToSymbol() + " " + L3.getToText() + " "
+													+ L3.getMultiBy() + "\n");
+											Log.d("FIVESTEP", R2.getFromSymbol() + " " + R2.getFromText() + " " + R2.getToSymbol() + " " + R2.getToText() + " "
+													+ R2.getMultiBy() + "\n");
+											Log.d("FIVESTEP",
+													R.getFromSymbol() + " " + R.getFromText() + " " + R.getToSymbol() + " " + R.getToText() + " "
+															+ R.getMultiBy() + "\n");
+
+											return Double.parseDouble(L.getMultiBy()) * Double.parseDouble(R.getMultiBy())
+													* Double.parseDouble(L2.getMultiBy()) * Double.parseDouble(R2.getMultiBy())
+													* Double.parseDouble(L3.getMultiBy());
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return 0;
+	}
+}
--- onelineconverter/src/com/mattlykins/onelineconverter/MainActivity.java
+++ onelineconverter/src/com/mattlykins/onelineconverter/MainActivity.java
@@ -1,16 +1,11 @@
 package com.mattlykins.onelineconverter;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-import com.mattlykins.onelineconverter.dbContract.dBase;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,7 +14,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener
 {
@@ -28,9 +22,7 @@ public class MainActivity extends Activity implements OnClickListener
 	Button bConvert;
 	TextView tvOutput;
 	String sEntry, sValue, sFromUnit, sToUnit;
-	Context context = this;
-
-	dbHelper mydbHelper = new dbHelper(this);
+	DBFunctions dbFunctions = new DBFunctions(this, new dbHelper(this));
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -123,7 +115,7 @@ public class MainActivity extends Activity implements OnClickListener
 						//Call conversion and return conversion factor
 						
 						Log.d("FERRET","Calling junkwrapper with " + sFromUnit + " " + sToUnit + "\n");
-						Double result =	FindFactorWrapper(FromTokens[j],ToTokens[j]);
+						Double result =	dbFunctions.FindFactorWrapper(this, FromTokens[j],ToTokens[j]);
 						Log.d("FERRET","Junkwrapper returned " + result + "\n");
 						
 						if( j != 0 && FromTokens[j-1].equals("/") && ToTokens[j-1].equals("/"))
@@ -151,253 +143,6 @@ public class MainActivity extends Activity implements OnClickListener
 		{
 
 		}
-
-	}
-	
-	private double FindFactorWrapper(String sFromUnit,String sToUnit)
-	{
-		// Verify the unit conversion is potentially possible
-		Cursor testFF = mydbHelper.searchFrom(sFromUnit, null);
-		Cursor testTF = mydbHelper.searchTo(sFromUnit, null);
-		if (testFF == null && testTF == null)
-		{
-			Toast toast = Toast.makeText(this, "Invalid From Unit", Toast.LENGTH_SHORT);
-			toast.show();
-			return 0;
-		}
-
-		Cursor testFT = mydbHelper.searchFrom(sToUnit, null);
-		Cursor testTT = mydbHelper.searchTo(sToUnit, null);
-		if (testFT == null && testTT == null)
-		{
-			Toast toast = Toast.makeText(this, "Invalid To Unit", Toast.LENGTH_SHORT);
-			toast.show();
-			return 0;
-		}
-
-		List<Integer> IDS = new ArrayList<Integer>();
-		
-		
-		Log.d("FERRET","Calling junk with " + sFromUnit + " " + sToUnit + "\n");
-		return(FindFactor(sFromUnit,sToUnit,IDS));	
-	}
-
-	private List<Convs> AddToList(Cursor cursor, List<Integer> IDS)
-	{
-		List<Convs> List1 = new ArrayList<Convs>();
-		for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext())
-		{
-			if (cursor != null)
-			{
-				boolean lgAddToList = true;
-				Convs conv = new Convs(cursor.getString(dBase.NDEX_ID), cursor.getString(dBase.NDEX_FROMSYMBOL), cursor.getString(dBase.NDEX_FROMTEXT),
-						cursor.getString(dBase.NDEX_TOSYMBOL), cursor.getString(dBase.NDEX_TOTEXT), cursor.getString(dBase.NDEX_MULTIBY));				
-//				for (int i : IDS)
-//				{
-//					if (i == Integer.parseInt(cursor.getString(dBase.NDEX_ID)))
-//					{
-//						lgAddToList = false;
-//						break;
-//					}
-//				}
-				//Investigate IDS system - maybe remove
-				if (lgAddToList)
-				{
-					Log.d("FERRET", cursor.getString(dBase.NDEX_ID) + " " + cursor.getString(dBase.NDEX_FROMSYMBOL) + " " + cursor.getString(dBase.NDEX_FROMTEXT)
-							+ " " + cursor.getString(dBase.NDEX_TOSYMBOL) + " " + cursor.getString(dBase.NDEX_TOTEXT) + " " + cursor.getString(dBase.NDEX_MULTIBY)
-							+ "\n");
-					List1.add(conv);
-					int id = Integer.parseInt(cursor.getString(dBase.NDEX_ID));
-					IDS.add(id);
-				}
-			}
-
-		}
-		return List1;
-	}
-
-	private double FindFactor(String sFromUnit, String sToUnit, List<Integer> IDS)
-	{
-		// List<Integer> IDS = new ArrayList<Integer>();
-
-		// Create a list of convs where the From unit in the db query is the
-		// user entered from unit
-		List<Convs> FF = new ArrayList<Convs>();
-		Cursor cursorFF = mydbHelper.searchFrom(sFromUnit, null);
-		if (cursorFF != null)
-		{
-			FF = AddToList(cursorFF, IDS);
-			cursorFF.close();
-
-			// Check the array of convs to see if any of the dB To units equal
-			// the user entered To Unit
-			for (Convs C : FF)
-			{
-				if (C.getToSymbol().equals(sToUnit))
-				{
-					return Double.parseDouble(C.getMultiBy());
-				}
-
-			}
-		}
-
-		// Query the user To unit from the To side of db
-		List<Convs> TT = new ArrayList<Convs>();
-		Cursor cursorTT = mydbHelper.searchTo(sToUnit, null);
-		if (cursorTT != null)
-		{
-			TT = AddToList(cursorTT, IDS);
-			cursorTT.close();
-
-			for (Convs L : FF)
-			{
-				for (Convs R : TT)
-				{
-					if (L.getToSymbol().equals(R.getFromSymbol()))
-					{
-
-						return Double.parseDouble(L.getMultiBy()) * Double.parseDouble(R.getMultiBy());
-					}
-
-				}
-			}
-		}
-
-		// The three step
-		for (Convs L : FF)
-		{
-			List<Convs> FF2 = new ArrayList<Convs>();
-			Cursor cursorFF2 = mydbHelper.searchFrom(L.getToSymbol(), null);
-			if (cursorFF2 != null)
-			{
-				FF2 = AddToList(cursorFF2, IDS);
-				cursorFF2.close();
-				for (Convs R : TT)
-				{
-					for (Convs L2 : FF2)
-					{
-						if (L.getToSymbol().equals(L2.getFromSymbol()) && R.getFromSymbol().equals(L2.getToSymbol()))
-						{
-							Log.d("THREESTEP", L.getFromSymbol() + " " + L.getFromText() + " " + L.getToSymbol() + " " + L.getToText() + " " + L.getMultiBy()
-									+ "\n");							
-							Log.d("THREESTEP",
-									L2.getFromSymbol() + " " + L2.getFromText() + " " + L2.getToSymbol() + " " + L2.getToText() + " " + L2.getMultiBy() + "\n");
-							Log.d("THREESTEP", R.getFromSymbol() + " " + R.getFromText() + " " + R.getToSymbol() + " " + R.getToText() + " " + R.getMultiBy()
-									+ "\n");
-
-							return Double.parseDouble(L.getMultiBy()) * Double.parseDouble(R.getMultiBy()) * Double.parseDouble(L2.getMultiBy());
-						}
-					}
-				}
-			}
-		}
-
-		// The four step
-		for (Convs L : FF)
-		{
-			List<Convs> FF2 = new ArrayList<Convs>();
-			Cursor cursorFF2 = mydbHelper.searchFrom(L.getToSymbol(), null);
-			if (cursorFF2 != null)
-			{
-				FF2 = AddToList(cursorFF2, IDS);
-				cursorFF2.close();
-				for (Convs R : TT)
-				{
-					List<Convs> TT2 = new ArrayList<Convs>();
-					Cursor cursorTT2 = mydbHelper.searchTo(R.getFromSymbol(), null);
-					if (cursorTT2 != null)
-					{
-						TT2 = AddToList(cursorTT2, IDS);
-						cursorTT2.close();
-						for (Convs L2 : FF2)
-						{
-							for (Convs R2 : TT2)
-							{
-								if (L.getToSymbol().equals(L2.getFromSymbol()) && R.getFromSymbol().equals(R2.getToSymbol())
-										&& L2.getToSymbol().equals(R2.getFromSymbol()))
-								{
-									Log.d("FOURSTEP",
-											L.getFromSymbol() + " " + L.getFromText() + " " + L.getToSymbol() + " " + L.getToText() + " " + L.getMultiBy()
-													+ "\n");
-									Log.d("FOURSTEP",
-											L2.getFromSymbol() + " " + L2.getFromText() + " " + L2.getToSymbol() + " " + L2.getToText() + " " + L2.getMultiBy()
-													+ "\n");
-									Log.d("FOURSTEP",
-											R2.getFromSymbol() + " " + R2.getFromText() + " " + R2.getToSymbol() + " " + R2.getToText() + " " + R2.getMultiBy()
-													+ "\n");
-									Log.d("FOURSTEP",
-											R.getFromSymbol() + " " + R.getFromText() + " " + R.getToSymbol() + " " + R.getToText() + " " + R.getMultiBy()
-													+ "\n");									
-
-									return Double.parseDouble(L.getMultiBy()) * Double.parseDouble(R.getMultiBy()) * Double.parseDouble(L2.getMultiBy())
-											* Double.parseDouble(R2.getMultiBy());
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
-		// The Five Step
-		for (Convs L : FF)
-		{
-			List<Convs> FF2 = new ArrayList<Convs>();
-			Cursor cursorFF2 = mydbHelper.searchFrom(L.getToSymbol(), null);
-			if (cursorFF2 != null)
-			{
-				FF2 = AddToList(cursorFF2, IDS);
-				cursorFF2.close();
-				for (Convs R : TT)
-				{
-					List<Convs> TT2 = new ArrayList<Convs>();
-					Cursor cursorTT2 = mydbHelper.searchTo(R.getFromSymbol(), null);
-					if (cursorTT2 != null)
-					{
-						TT2 = AddToList(cursorTT2, IDS);
-						cursorTT2.close();
-						for (Convs L2 : FF2)
-						{
-							List<Convs> FF3 = new ArrayList<Convs>();
-							Cursor cursorFF3 = mydbHelper.searchFrom(L2.getToSymbol(), null);
-							if (cursorFF3 != null)
-							{
-								FF3 = AddToList(cursorFF3, IDS);
-								cursorFF3.close();
-
-								for (Convs R2 : TT2)
-								{
-									for (Convs L3 : FF3)
-									{
-
-										if (L.getToSymbol().equals(L2.getFromSymbol()) && R.getFromSymbol().equals(R2.getToSymbol())
-												&& L2.getToSymbol().equals(L3.getFromSymbol()) && L3.getToSymbol().equals(R2.getFromSymbol()))
-										{
-											Log.d("FIVESTEP", L.getFromSymbol() + " " + L.getFromText() + " " + L.getToSymbol() + " " + L.getToText() + " "
-													+ L.getMultiBy() + "\n");
-											Log.d("FIVESTEP", L2.getFromSymbol() + " " + L2.getFromText() + " " + L2.getToSymbol() + " " + L2.getToText() + " "
-													+ L2.getMultiBy() + "\n");
-											Log.d("FIVESTEP", L3.getFromSymbol() + " " + L3.getFromText() + " " + L3.getToSymbol() + " " + L3.getToText() + " "
-													+ L3.getMultiBy() + "\n");
-											Log.d("FIVESTEP", R2.getFromSymbol() + " " + R2.getFromText() + " " + R2.getToSymbol() + " " + R2.getToText() + " "
-													+ R2.getMultiBy() + "\n");
-											Log.d("FIVESTEP", R.getFromSymbol() + " " + R.getFromText() + " " + R.getToSymbol() + " " + R.getToText() + " "
-													+ R.getMultiBy() + "\n");											
-
-											return Double.parseDouble(L.getMultiBy()) * Double.parseDouble(R.getMultiBy())
-													* Double.parseDouble(L2.getMultiBy()) * Double.parseDouble(R2.getMultiBy())
-													* Double.parseDouble(L3.getMultiBy());
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return 0;
 
 	}
 }
